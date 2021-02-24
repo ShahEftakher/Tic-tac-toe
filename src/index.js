41729cad0f3a9fbc98126ee5e272c81d47521857
ShahEftakher <shaheftakher@iut-dhaka.edu>
2021-02-24 15:46:54 +0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,101 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//square component
+//state is controlled by <Board/> 
+//so it is a controlled component
+class Square extends React.Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          //for updating the state
+          //on this update the square component is re rendered with the updated value
+          this.props.onClick();
+        }}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Board component
+//board will track the app state
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    //initializing each box with null
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => {
+          this.handleClick(i);
+        }}
+      />
+    );
+  }
+
+  handleClick(i) {
+    //copy the whole array
+    const squares = this.state.squares.slice();
+    //update a element
+    squares[i] = "X";
+    //update the state
+    //this will update the parent state(<Board/>)
+    //as <Square/> is sharing state with its parent <Board/> it will re render as well
+    this.setState({ squares: squares });
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)} {/* === <Square value={0}>*/}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+//Game component
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
